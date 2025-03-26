2a416fe9638dbb1fcaa42390745f4059b4a3f4c8
chouaibAtm <atmanichouaib22@gmail.com>
2025-03-26 18:01:39 +0100

    add TypeScript declarations for SVG imports and update Metro config for SVG support

--- NticSave/metro.config.js
+++ NticSave/metro.config.js
@@ -1,6 +1,19 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require("expo/metro-config")
+const { withNativeWind } = require("nativewind/metro")
 
-const config = getDefaultConfig(__dirname);
+// First get the default config
+const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: "./app/global.css" });
+// Update transformer and resolver
+const { transformer, resolver } = config
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+}
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
+}
+
+module.exports = withNativeWind(config, { input: "./app/global.css" })
